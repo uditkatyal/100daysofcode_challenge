68676e7d2f049b81b2c627f99445708bf5fdd875
uditkatyal <uditkatyal@gmail.com>
2021-01-08 22:54:10 +0530

    initial commit

---
+++ day1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value :";
+    cin>>n;
+
+    bool flag =0;
+
+    for(int i=2; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"Non Prime";
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<"Prime Number";
+    }
+    return 0;
+}
