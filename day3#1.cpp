c006748f9e75986512e4132c6044b802e63cf646
uditkatyal <uditkatyal@gmail.com>
2021-01-20 00:17:12 +0530

    complete

---
+++ day3#1.cpp
@@ -0,0 +1,23 @@
+// pattern question - to print a rectangle 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+    int columns;
+    cout<<"Enter the rows :";
+    cin>>rows;
+    cout<<"Enter the columns :";
+    cin>>columns;
+     
+    for(int i=1; i<=rows; i++)
+      {
+          for(int j=1; j<=columns; j++)
+          {
+              cout<<" * ";
+          }
+    cout<<endl;
+      }
+      return 0;
+}
